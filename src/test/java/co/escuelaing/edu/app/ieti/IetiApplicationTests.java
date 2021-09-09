c385c319b307095e6ee2ce7b5afdedaf9e4e475f
Sebastian Herrera <sebastianhh1999@gmail.com>
2021-09-09 13:26:54 -0500

    Initial commit

---
+++ src/test/java/co/escuelaing/edu/app/ieti/IetiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.escuelaing.edu.app.ieti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IetiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
