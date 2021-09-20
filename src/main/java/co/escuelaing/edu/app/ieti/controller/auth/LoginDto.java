0dcc505a0ff43a7dc9526d49b09b01ad75a994e0
Johan Rueda <johan.rueda@mail.escuelaing.edu.co>
2021-09-20 17:37:15 -0500

    implementando autenticacion de usuario con token, falta terminar usuario para finalizar seguridad

---
+++ src/main/java/co/escuelaing/edu/app/ieti/controller/auth/LoginDto.java
@@ -0,0 +1,25 @@
+package co.escuelaing.edu.app.ieti.controller.auth;
+
+public class LoginDto
+{
+    String email;
+
+    String password;
+
+    public LoginDto( String email, String password )
+    {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+}
+
