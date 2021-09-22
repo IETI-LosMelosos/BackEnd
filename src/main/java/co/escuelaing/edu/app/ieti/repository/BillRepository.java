bc3676f670a1c50e20143e19528570d7585ee382
Sebastian Herrera <sebastianhh1999@gmail.com>
2021-09-22 10:45:26 -0500

    Fix repo

--- src/main/java/co/escuelaing/edu/app/ieti/repository/BillRepository.java
+++
@@ -1,7 +0,0 @@
-package co.escuelaing.edu.app.ieti.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BillRepository extends MongoRepository<Bill,String>{
-    
-}
--- src/main/java/co/escuelaing/edu/app/ieti/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package co.escuelaing.edu.app.ieti.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User,String>{
-    
-}
